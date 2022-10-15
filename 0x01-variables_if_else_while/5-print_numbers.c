0e316af3a1efdd2a3a9ba7f295382df8ffb13f3b
David <davidadje070@gmail.com>
2022-10-14 17:36:46 -0700

    adding 5-file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* more headers goes there */
+
+/**
+ * main - prints results
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
