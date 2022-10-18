f77608608767eef0e44b1818ca94047aa20a6bb8
David <davidadje070@gmail.com>
2022-10-18 10:36:41 -0700

    adding 1-file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - check code
+  * Return: return 0
+  */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+
+	return (0);
+}
