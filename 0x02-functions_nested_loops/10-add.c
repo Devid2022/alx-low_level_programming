1e4aea9623114852cde4713d8963f958571bdb68
David <davidadje070@gmail.com>
2022-10-18 18:44:39 -0700

    adding 10-file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - display the sum of two integer
+  * @a: parameter to make test
+  * @b: parameter to make test
+  * Return: return 0
+  */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
