e99cfa911f3dd013fab9710c7e9eda480ca0c76b
David <davidadje070@gmail.com>
2022-10-26 23:33:55 -0700

    adding 0-file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * *_strcat - function that concatenates two strings
+  * @dest: input
+  * @src: input
+  * Return: return a char
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	
+	return (strcat(dest,src));
+}
