807295fa0eebbce7c2f8737e1cca1a4d16dd5133
David <davidadje070@gmail.com>
2022-11-02 01:07:40 -0700

    adding 0-file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _puts_recursion - function that print a string 
+  * @s: pointer
+  * Return: nothing
+  */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
